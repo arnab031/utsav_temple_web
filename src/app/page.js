233ecfac0bf43ce42cec8fe1c5bbe95fb17c47d8
arnab031 <51916075+arnab031@users.noreply.github.com>
2025-03-09 00:38:33 +0530

    added log

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
+  console.log('Hello world!');
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline text-purple-600">
         Hello world!
       </h1>
       <div>
-        
         <Button>Click me</Button>
       </div>
     </main>
